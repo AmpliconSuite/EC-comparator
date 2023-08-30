33f2ae44ae7f3d081ac442e6d73eb70c6957dd0f
Madalina Giurgiu <Madalina.Giurgiu@charite.de>
2023-08-30 10:33:42 -0700

    Convert report to pdf

--- src/utils/report.py
+++ src/utils/report.py
@@ -8,6 +8,7 @@ Create report with all the results
 import os
 import datetime
 from jinja2 import Environment, FileSystemLoader
+from xhtml2pdf import pisa
 from src.utils.utils import OUTFILES as o
 
 def generate_report(path_s1,
@@ -18,21 +19,29 @@ def generate_report(path_s1,
     if outdir:
         env = Environment(loader=FileSystemLoader('.'))
         template = env.get_template("utils/report/results.html")
-
         html = template.render(page_title_text='AmpliconComparison report',
                                description_time=datetime.datetime.now(),
                                description_s1=path_s1,
                                description_s2=path_s2,
                                description_output=outdir,
                                total_cost=total_cost,
-                               path_total_cost=os.path.join(outdir, o.TOTAL_COST_PNG),
-                               path_total_table=os.path.join(outdir, o.TOTAL_COST_TABLE),
-                               path_plot_cn_sv=os.path.join(outdir, o.COVERAGE_BREAKPOINTS_PROFILE))
+                               path_total_cost=o.TOTAL_COST_PNG,
+                               path_total_table=o.TOTAL_COST_TABLE,
+                               path_plot_cn_sv=o.COVERAGE_BREAKPOINTS_PROFILE)
 
         # 5. Write the template to an HTML file
-        outfile = os.path.join(outdir, 'report.html')
+        outdirabs = os.path.abspath(outdir)
+        outfile = os.path.join(outdirabs, 'report.html')
         with open(outfile, 'w') as f:
             f.write(html)
+
+        # 6. Convert report to pdf
+        outfile = os.path.join(outdirabs, 'report.pdf')
+        os.chdir(outdirabs)
+        with open(outfile, "w+b") as out_pdf_file_handle:
+            pisa.CreatePDF(
+                src=html,  # HTML to convert
+                dest=out_pdf_file_handle)  # File handle to receive result
 
 if __name__ == "__main__":
     generate_report("/Users/madag/Projects/PhD/github/ecdna-compare/examples/COLODM320/COLO320DM_Hung2021_amplicon3_cycles.bed",
