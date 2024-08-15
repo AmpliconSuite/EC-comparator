refactor and fix the calculation for hamming distance and breakpoint distance

src/metrics/compare.py
@@ -19,7 +19,7 @@ from src.utils.utils import HEADER as ht
 from src.utils.utils import DDT as ddt
 from src.utils.utils import OUTFILES as o
 from src.utils.utils import NpEncoder
-from src.utils import report
+# from src.utils import report
 
 
 import warnings
@@ -98,17 +98,19 @@ def compare_cycles(t_file, r_file, outdir, dict_configs, plot=True):
 
 	# as pyranges objects
 	df_t_pr, df_r_pr, df_bins_pr = read_pyranges(df_t, df_r, bins)
+	# as dataframe
+	df_ = overlap_vector(df_t_pr, df_r_pr, df_bins_pr)
 
 	# 2. Compute hamming distance and others
-	h = get_hamming_score(df_t_pr, df_r_pr, df_bins_pr)
-	h_norm = get_hamming_score_norm(df_t_pr, df_r_pr, df_bins_pr)
+	df_, h = get_hamming_score(df_)
+	h_norm = get_hamming_score_norm(df_)
 
 	dict_metrics[ht.DISTANCES][ddt.HAMMING] = round(h,2)
 	dict_metrics[ht.DISTANCES][ddt.HAMMING_NORM] = round(h_norm,2)
 
 	# 3. Compute copy-number similarity
-	cn_profile_t = get_feature_cn(df_t, bins)
-	cn_profile_r = get_feature_cn(df_r, bins)
+	cn_profile_t = get_feature_cn(df_t, df_)
+	cn_profile_r = get_feature_cn(df_r, df_)
 
 	cv_distance = get_cosine_distance_cn(cn_profile_t, cn_profile_r)
 	dict_metrics[ht.DISTANCES][ddt.COSINE_DISTANCE] = round(cv_distance,2)
@@ -124,8 +126,10 @@ def compare_cycles(t_file, r_file, outdir, dict_configs, plot=True):
 	# dict_metrics[ht.DISTANCES][ddt.JACCARD_DISTANCE] = 1
 
 	# 5. Penalize for cycles and fragments multiplicity (if the tool decompose in one or more cycles)
-	overlap_fragments_distance = get_overlap_fragments_weighted(df_t_pr, df_r_pr, df_bins_pr)
-	overlap_cycles_distance = get_overlap_cycles_weighted(df_t_pr, df_r_pr, df_bins_pr)
+	# overlap_fragments_distance = get_overlap_fragments_weighted(df_t_pr, df_r_pr, df_bins_pr)
+	# overlap_cycles_distance = get_overlap_cycles_weighted(df_t_pr, df_r_pr, df_bins_pr)
+	overlap_fragments_distance = get_overlap_fragments_weighted(df_)
+	overlap_cycles_distance = get_overlap_cycles_weighted(df_t, df_r,  df_)
 	dict_metrics[ht.DISTANCES][ddt.FRAGMENTS_DISTANCE] = round(overlap_fragments_distance,2)
 	dict_metrics[ht.DISTANCES][ddt.CYCLES_DISTANCE] = round(overlap_cycles_distance,2)
 
@@ -137,43 +141,43 @@ def compare_cycles(t_file, r_file, outdir, dict_configs, plot=True):
 	dict_metrics[ht.DISTANCES][ddt.TOTAL_COST_DESCRIPTION] = total_cost_description
 	print("Total cost:",total_cost)
 
-	# 8. Output
-	if outdir:
-		with open(os.path.join(outdir, o.METRICS_JSON), 'w', encoding='utf-8') as f:
-			final_dict = remove_ref2methods(dict_metrics)
-			json.dump(final_dict, f, ensure_ascii=False, indent=4, cls=NpEncoder)
-
-	# plot total cost
-	if plot:
-		outfile = os.path.join(outdir, o.TOTAL_COST_PNG) if outdir else None
-		viz.draw_total_cost(dict_metrics, outfile)
-		outfile = os.path.join(outdir, o.TOTAL_COST_TABLE) if outdir else None
-		viz.draw_total_cost_table(dict_metrics, outfile)
-
-	# save and plot coverage profile
-	if outdir:
-		cn_profile_t.to_csv(os.path.join(outdir, o.COVERAGE_PROFILE_S1_TXT), header=True, index=False, sep="\t")
-		cn_profile_r.to_csv(os.path.join(outdir, o.COVERAGE_PROFILE_S2_TXT), header=True, index=False, sep="\t")
-	if plot:
-		outfile = os.path.join(outdir, o.COVERAGE_PROFILE_PNG) if outdir else None
-		viz.draw_cn(cn_profile_t, cn_profile_r, chrlist, outfile=outfile)
-
-	# save breakpoints profile
-	if outdir:
-		br_t.to_csv(os.path.join(outdir, o.BREAKPOINTS_PROFILE_S1_TXT), header=True, index=False, sep="\t")
-		br_r.to_csv(os.path.join(outdir, o.BREAKPOINTS_PROFILE_S2_TXT), header=True, index=False, sep="\t")
-
-	# plot merged coverage and breakpoint profile
-	max_coverage = max(np.max(cn_profile_t[ht.CN].tolist()), np.max(cn_profile_r[ht.CN].tolist()))
-	max_coverage = max_coverage + 0.5 * max_coverage
-	if plot:
-		outfile = os.path.join(outdir, o.COVERAGE_BREAKPOINTS_PROFILE) if outdir else None
-		viz.plot_combined(br_t, br_r, cn_profile_t, cn_profile_r, breakpoint_matches, chrlist, max_coverage, outfile=outfile)
-
-	# create report
-	if outdir:
-		report.generate_report(
-			t_file,
-			r_file,
-			outdir=outdir,
-			total_cost=total_cost)
+	# # 8. Output
+	# if outdir:
+	# 	with open(os.path.join(outdir, o.METRICS_JSON), 'w', encoding='utf-8') as f:
+	# 		final_dict = remove_ref2methods(dict_metrics)
+	# 		json.dump(final_dict, f, ensure_ascii=False, indent=4, cls=NpEncoder)
+	#
+	# # plot total cost
+	# if plot:
+	# 	outfile = os.path.join(outdir, o.TOTAL_COST_PNG) if outdir else None
+	# 	viz.draw_total_cost(dict_metrics, outfile)
+	# 	outfile = os.path.join(outdir, o.TOTAL_COST_TABLE) if outdir else None
+	# 	viz.draw_total_cost_table(dict_metrics, outfile)
+	#
+	# # save and plot coverage profile
+	# if outdir:
+	# 	cn_profile_t.to_csv(os.path.join(outdir, o.COVERAGE_PROFILE_S1_TXT), header=True, index=False, sep="\t")
+	# 	cn_profile_r.to_csv(os.path.join(outdir, o.COVERAGE_PROFILE_S2_TXT), header=True, index=False, sep="\t")
+	# if plot:
+	# 	outfile = os.path.join(outdir, o.COVERAGE_PROFILE_PNG) if outdir else None
+	# 	viz.draw_cn(cn_profile_t, cn_profile_r, chrlist, outfile=outfile)
+	#
+	# # save breakpoints profile
+	# if outdir:
+	# 	br_t.to_csv(os.path.join(outdir, o.BREAKPOINTS_PROFILE_S1_TXT), header=True, index=False, sep="\t")
+	# 	br_r.to_csv(os.path.join(outdir, o.BREAKPOINTS_PROFILE_S2_TXT), header=True, index=False, sep="\t")
+	#
+	# # plot merged coverage and breakpoint profile
+	# max_coverage = max(np.max(cn_profile_t[ht.CN].tolist()), np.max(cn_profile_r[ht.CN].tolist()))
+	# max_coverage = max_coverage + 0.5 * max_coverage
+	# if plot:
+	# 	outfile = os.path.join(outdir, o.COVERAGE_BREAKPOINTS_PROFILE) if outdir else None
+	# 	viz.plot_combined(br_t, br_r, cn_profile_t, cn_profile_r, breakpoint_matches, chrlist, max_coverage, outfile=outfile)
+	#
+	# # create report
+	# if outdir:
+	# 	report.generate_report(
+	# 		t_file,
+	# 		r_file,
+	# 		outdir=outdir,
+	# 		total_cost=total_cost)

src/metrics/distances.py
@@ -18,80 +18,86 @@ import pyranges as pr
 
 from src.utils.utils import HEADER as ht
 from src.utils.utils import DDT as ddt
+from src.utils.utils import PROPS as p
+from src.metrics.features import rename_columns
 
-def get_hamming_score(e1, e2, bins):
+def get_hamming_score(df_):
 	"""
 	Compute hamming distance between the two reconstructions
 
 	Args:
-		e1 (pd.DataFrame): First reconstruction
-		e2 (pd.DataFrame): Second reconstruction
-		bins (pd.DataFrame): Bins of the intervals union e1 and e2
+		df_(pd.DataFrame):
+
 	"""
-	grs = {n: s for n, s in zip(["bins", "e1", "e2"], [bins, e1, e2])}
-	# Chromosome	Start	End	bins	e1	e2
-	overlaps = pr.count_overlaps(grs)
-	overlaps = overlaps.as_df()
+	df_[ddt.HAMMING] = df_.apply(lambda x: np.logical_xor(x.h1, x.h2), axis=1)
+	df_[ddt.HAMMING] = df_[ddt.HAMMING].astype(int)
+	df_[ddt.PROD] = df_[ddt.HAMMING] * (df_[ht.CEND] - df_[ht.CSTART])
+	df_[ddt.LEN] = abs(df_[ht.CEND] - df_[ht.CSTART]) * df_[ht.BIN_ENABLED]
 
-	# everything > 1 set to 1
-	overlaps['e1'] = overlaps['e1'].apply(lambda x: 1 if x >= 1 else 0)
-	overlaps['e2'] = overlaps['e2'].apply(lambda x: 1 if x >= 1 else 0)
-
-	overlaps['hamming'] = overlaps.apply(lambda x: np.logical_xor(x.e1, x.e2), axis=1)
-	overlaps['hamming'] = overlaps['hamming'].astype(int)
-
-	overlaps["prod"] = overlaps["hamming"] * (overlaps["End"] - overlaps["Start"])
-	return overlaps["prod"].sum()
+	return df_, df_[ddt.PROD].sum()
 
 
-def get_hamming_score_norm(e1, e2, bins):
+def get_hamming_score_norm(df_):
 	"""
 	Compute hamming distance between the two reconstructions
 
 	Args:
-		e1 (pd.DataFrame): First reconstruction
-		e2 (pd.DataFrame): Second reconstruction
-		bins (pd.DatamFrame): Bins of the intervals union e1 and e2
+		df_(pd.DataFrame)
 	"""
-	grs = {n: s for n, s in zip(["bins", "e1", "e2"], [bins, e1, e2])}
-	# Chromosome	Start	End	bins	e1	e2
-	overlaps = pr.count_overlaps(grs)
-	overlaps = overlaps.as_df()
 
-	# everything > 1 set to 1
-	overlaps['e1'] = overlaps['e1'].apply(lambda x: 1 if x >= 1 else 0)
-	overlaps['e2'] = overlaps['e2'].apply(lambda x: 1 if x >= 1 else 0)
+	# df_['hamming'] = df_.apply(lambda x: np.logical_xor(x.h1, x.h2), axis=1)
+	# df_['hamming'] = df_['hamming'].astype(int)
+	#
+	# # consider only bins used in the reconstruction of s1 or s2
+	# df_["len"] = abs(df_["End"] - df_["Start"]) * df_["bins"]
+	# df_["prod"] = df_["hamming"] * df_["len"]
 
-	overlaps['hamming'] = overlaps.apply(lambda x: np.logical_xor(x.e1, x.e2), axis=1)
-	overlaps['hamming'] = overlaps['hamming'].astype(int)
-
-	overlaps["len"] = abs(overlaps["End"] - overlaps["Start"])
-	overlaps["prod"] = overlaps["hamming"] * overlaps["len"]
-
-	return (overlaps["prod"].sum()) / (overlaps["len"].sum())
+	return (df_[ddt.PROD].sum()) / (df_[ddt.LEN].sum())
 
 
-def get_overlap_fragments_weighted(e1, e2, bins):
+def get_overlap_fragments_weighted(df_):
 	"""
 	Compute the overlap distance between the fragment counts of two reconstructions which overlap every genomic bin.
 
 	Args:
-		e1 (pd.DataFrame): First reconstruction
-		e2 (pd.DataFrame): Second reconstruction
-		bins (pd.DatamFrame): Bins of the intervals union e1 and e2
+		df_ (pd.DataFrame): Binned summary reconstructions
 	"""
-	grs = {n: s for n, s in zip(["bins", "e1", "e2"], [bins, e1, e2])}
-	# Chromosome	Start	End	bins	e1	e2
-	overlaps = pr.count_overlaps(grs)
-	overlaps = overlaps.as_df()
-	overlaps["overlapping_score"] = overlaps.apply(lambda x: abs(x.e1 - x.e2), axis=1)
-	overlaps["len"] = abs(overlaps["End"] - overlaps["Start"])
+	df_[ddt.OVERLAP_SCORE] = df_.apply(lambda x: abs(x.e1 - x.e2), axis=1)
+	# important to disable unused genomic bins
+	df_[ddt.LEN] = abs(df_[ht.CEND] - df_[ht.CSTART]) * df_[ht.BIN_ENABLED]
+
 	# account for duplicated fragments
-	overlaps["totallen"] = overlaps.apply(lambda x: max(x.e1, x.e2) * x.len, axis=1)
-	overlaps["prod"] = overlaps["overlapping_score"] * overlaps["len"]
+	df_[ddt.TOTALLEN] = df_.apply(lambda x: max(x.e1, x.e2) * x.len, axis=1)
+	df_[ddt.PROD] = df_[ddt.OVERLAP_SCORE] * df_[ddt.LEN]
 
-	return (overlaps["prod"].sum()) / (overlaps["totallen"].sum())
+	return (df_[ddt.PROD].sum()) / (df_[ddt.TOTALLEN].sum())
 
+def count_cycles(results, e1,bins,c, col):
+	"""
+	Args:
+		results (pd.DataFrame):
+		e1 (pd.DataFrame): Amplicon reconstructions
+		bins (pd.DataFrame): Reference genomic bins
+		c (str): Cycle_id
+		col (str): Has value 's1' or 's2'
+	"""
+
+	# filter pyrange by circle name
+	df_tmp = e1[e1[ht.CIRC_ID] == c]
+	df_tmp_pr = pr.PyRanges(
+		df_tmp.rename(columns=rename_columns(df_tmp.columns.tolist(), p.DICT_COLS), errors="raise", inplace=False))
+
+	# compute overlap
+	grs = {n: s for n, s in zip([ht.BINS, col], [bins, df_tmp_pr])}
+	overlap = pr.count_overlaps(grs).as_df()
+	# mark bin as covered by cycle c
+	overlap[col] = overlap[col].apply(lambda x: 1 if x >= 1 else 0)
+
+	# merge results
+	results = pd.concat([results, overlap]).groupby(
+		[ht.CCHR, ht.CSTART, ht.CEND], observed=True, as_index=False).sum()
+
+	return results
 
 def get_overlap_cycles_weighted(e1, e2, bins):
 	"""
@@ -100,84 +106,75 @@ def get_overlap_cycles_weighted(e1, e2, bins):
 	Args:
 		e1 (pd.DataFrame): First reconstruction
 		e2 (pd.DataFrame): Second reconstruction
-		bins (pd.DatamFrame): Bins of the intervals union e1 and e2
+		bins (pd.DataFrame): Bins of the intervals union e1 and e2
 	"""
-	c1 = e1.as_df()[ht.CIRC_ID].drop_duplicates().tolist()
-	c2 = e2.as_df()[ht.CIRC_ID].drop_duplicates().tolist()
+	pr_bins = pr.PyRanges(bins[[ht.CCHR,ht.CSTART,ht.CEND]])
+	c1 = e1[ht.CIRC_ID].drop_duplicates().tolist()
+	c2 = e2[ht.CIRC_ID].drop_duplicates().tolist()
 
-	overlap_parent1 = deepcopy(bins.as_df())
-	overlap_parent1["e1"] = 0
-	overlap_parent1["len"] = abs(overlap_parent1["End"] - overlap_parent1["Start"])
+	overlap_parent1 = deepcopy(bins)
+	overlap_parent1[ht.S1] = 0
+
+	# overlap_parent1[ddt.LEN] = abs(overlap_parent1[ht.CEND] - overlap_parent1[ht.CSTART])
 	for c in c1:
-		# filter pyrange by circle name
-		df_tmp = e1.as_df()
-		df_tmp = df_tmp[df_tmp[ht.CIRC_ID] == c]
-		df_tmp_pr = pr.PyRanges(df_tmp)
+		overlap_parent1 = count_cycles(overlap_parent1, e1, pr_bins, c, ht.S1)
 
-		# compute overlap
-		grs = {n: s for n, s in zip(["bins", "e1"], [bins, df_tmp_pr])}
-		overlap = pr.count_overlaps(grs).as_df()
-		overlap['e1'] = overlap['e1'].apply(lambda x: 1 if x >= 1 else 0)
-
-		# merge results
-		overlap_parent1 = pd.concat([overlap, overlap_parent1]).groupby(
-			["Chromosome", "Start", "End", "bins"]).sum().reset_index()
-
-	overlap_parent2 = deepcopy(bins.as_df())
-	overlap_parent2["e2"] = 0
+	overlap_parent2 = deepcopy(bins)
+	overlap_parent2[ht.S2] = 0
 
 	for c in c2:
-		# filter pyrange by circle name
-		df_tmp = e2.as_df()
-		df_tmp = df_tmp[df_tmp[ht.CIRC_ID] == c]
-		df_tmp_pr = pr.PyRanges(df_tmp)
+		overlap_parent2 = count_cycles(overlap_parent2, e2, pr_bins, c, ht.S2)
 
-		# compute overlap
-		grs = {n: s for n, s in zip(["bins", "e2"], [bins, df_tmp_pr])}
-		overlap = pr.count_overlaps(grs).as_df()
-		overlap['e2'] = overlap['e2'].apply(lambda x: 1 if x >= 1 else 0)
+	overlaps = pd.merge(overlap_parent1[[ht.CCHR, ht.CSTART, ht.CEND, ht.S1, ht.BIN_ENABLED]],
+						overlap_parent2[[ht.CCHR, ht.CSTART, ht.CEND, ht.S2, ht.BIN_ENABLED]], how='inner')
 
-		# merge results
-		overlap_parent2 = pd.concat([overlap, overlap_parent2]).groupby(
-			["Chromosome", "Start", "End", "bins"]).sum().reset_index()
+	overlaps[ddt.OVERLAP_SCORE] = overlaps.apply(lambda x: abs(x[ht.S1] - x[ht.S2]), axis=1)
+	overlaps[ddt.LEN] = abs(overlaps[ht.CEND] - overlaps[ht.CSTART]) * overlaps[ht.BIN_ENABLED]
+	overlaps[ddt.TOTALLEN] = overlaps.apply(lambda x: max(x[ht.S1], x[ht.S2]) * x.len, axis=1)
+	overlaps[ddt.PROD] = overlaps[ddt.OVERLAP_SCORE] * overlaps[ddt.LEN]
 
-	overlaps = pd.merge(overlap_parent1[["Chromosome", "Start", "End", "e1"]],
-						overlap_parent2[["Chromosome", "Start", "End", "e2"]], how='inner')
-
-	overlaps["overlapping_score"] = overlaps.apply(lambda x: abs(x.e1 - x.e2), axis=1)
-	overlaps["len"] = abs(overlaps["End"] - overlaps["Start"])
-	overlaps["totallen"] = overlaps.apply(lambda x: max(x.e1, x.e2) * x.len, axis=1)
-	overlaps["prod"] = overlaps["overlapping_score"] * overlaps["len"]
-
-	return (overlaps["prod"].sum()) / (overlaps["totallen"].sum())
+	return (overlaps[ddt.PROD].sum()) / (overlaps[ddt.TOTALLEN].sum())
 
 
 def get_cosine_distance_cn(cn_profile1, cn_profile2):
 	"""
 	Get cosine distance between the two copy number profiles
 	"""
-	cn_profile1["estimated_cn_normalized"] = cn_profile1.apply(lambda x: x[ht.CN] * abs(x[ht.END]-x[ht.START]), axis=1)
-	cn_profile2["estimated_cn_normalized"] = cn_profile2.apply(lambda x: x[ht.CN] * abs(x[ht.END]-x[ht.START]), axis=1)
+	cn_profile1["estimated_cn_normalized"] = cn_profile1.apply(lambda x: x[ht.CN] * abs(x[ht.CEND] - x[ht.CSTART]),
+															   axis=1)
+	cn_profile2["estimated_cn_normalized"] = cn_profile2.apply(lambda x: x[ht.CN] * abs(x[ht.CEND] - x[ht.CSTART]),
+															   axis=1)
 
-	return 1 - abs(cosine_similarity(np.array([cn_profile1["estimated_cn_normalized"].tolist()]),
-									 np.array([cn_profile2["estimated_cn_normalized"].tolist()]))[0][0])
+	# filter out bins which are disabled
+	cn_profile1_new = cn_profile1[cn_profile1[ht.BIN_ENABLED] == 1]
+	cn_profile2_new = cn_profile2[cn_profile2[ht.BIN_ENABLED] == 1]
+
+	a = np.array([cn_profile1_new["estimated_cn_normalized"].tolist()])
+	b = np.array([cn_profile2_new["estimated_cn_normalized"].tolist()])
+
+	return 1 - abs(cosine_similarity(a,b))[0][0]
+
 
 def get_jc_distance_cn(cn_profile1, cn_profile2):
 	"""
 	Get jaccard index distance between the two copy number profiles
 	"""
-	cn_merge = pd.merge(cn_profile1, cn_profile2, on=[ht.CHR, ht.START, ht.END])
-	cn_merge["max"] = cn_merge.apply(lambda x: max(x["estimated_cn_normalized_x"],x["estimated_cn_normalized_y"]),
-									  axis=1)
+	cn_merge = pd.merge(cn_profile1, cn_profile2, on=[ht.CCHR, ht.CSTART, ht.CEND, ht.BIN_ENABLED])
+	cn_merge["max"] = cn_merge.apply(lambda x: max(x["estimated_cn_normalized_x"], x["estimated_cn_normalized_y"]),
+									 axis=1)
 	# overlap
 	cn_merge["min"] = cn_merge.apply(lambda x: min(x["estimated_cn_normalized_x"], x["estimated_cn_normalized_y"]),
 									 axis=1)
+
+	# filter out unused bins
+	cn_merge = cn_merge[cn_merge[ht.BIN_ENABLED]==1]
 
 	# JC = matches / all
 	# 1 - JC means -> 0 highly similar, 1 - not similar
 	return 1 - cn_merge["min"].sum() / cn_merge["max"].sum()
 
-def euclidian_distance(cha, a, chb,  b, chx,x, chy, y):
+
+def euclidian_distance(cha, a, chb, b, chx, x, chy, y):
 	if cha == chx and chb == chy:
 		return math.sqrt((a - x) ** 2 + (b - y) ** 2)
 	elif cha == chy and chb == chx:
@@ -191,7 +188,6 @@ def euclidian_distance_norm_l2(a, b, x, y):
 	v2 = np.array([[x, y]])
 	v1_norm = preprocessing.normalize(v1, norm='l2')
 	v2_norm = preprocessing.normalize(v2, norm='l2')
-
 	return skl.euclidean_distances(v1_norm, v2_norm)
 
 
@@ -202,14 +198,12 @@ def euclidian_distance_norm_l1(a, b, x, y):
 	v2_norm = preprocessing.normalize(v2, norm='l1')
 	return skl.euclidean_distances(v1_norm, v2_norm)
 
-
 def auc_triangle(a, b, x, y):
 	return abs(a - x) * abs(b - y) / 2
 
 
 def auc_trapeze(a, b, x, y):
 	return (b + y) * abs(a - x) / 2
-
 
 def match_angle(a, b, x, y):
 	alpha = math.atan(abs(x - y) / abs(a - b))
@@ -280,3 +274,4 @@ dict_distance_paired_breakpoints_thresholds = {
 	ddt.MATCH_ANGLE: ddt.MATCH_ANGLE_THRESHOLD,
 	ddt.UNMATCHED: ddt.UNMATCHED_THRESHOLD
 }
+

src/metrics/features.py
@@ -119,55 +119,26 @@ def bin_genome(t_collection, r_collection, margin_size=10000):
 	Bin the intervals by the breakpoints union.
 	Warning: Setting a margin size can effect the output of different distances
 	"""
-	# # binning using pybedtools
-	# df_bins = pd.DataFrame(np.concatenate((t_collection[[ht.CHR, ht.START, ht.END]].values,
-	# 									   r_collection[[ht.CHR, ht.START, ht.END]].values),
-	# 									  axis=0))
-	# df_bins.columns = [ht.CHR, ht.START, ht.END]
-	# a = BedTool.from_dataframe(df_bins).sort()
-	# # merge bins 500 apart
-	# df_bins_merged = a.merge(d=0).to_dataframe()
-	# df_bins_merged.columns = [ht.CHR, ht.START, ht.END]
-	# df_bins_merged[ht.LEN] = df_bins_merged.apply(lambda x: abs(x[ht.START] - x[ht.END]), axis=1)
-	# chrlist = df_bins[ht.CHR].drop_duplicates().tolist()
-	#
-	# print(df_bins_merged)
-	# print(chrlist)
-	#
-	# return df_bins_merged, chrlist
 
 	df_bins = pd.DataFrame(np.concatenate((t_collection[[ht.CHR, ht.START]].values,
 										   t_collection[[ht.CHR, ht.END]].values,
 										   r_collection[[ht.CHR, ht.START]].values,
 										   r_collection[[ht.CHR, ht.END]].values),
-										  axis=0))
+										  axis=0)).drop_duplicates()
 
 	df_bins.columns = [ht.CHR, ht.START]
-	df_bins[ht.START] = df_bins[ht.START]
-	df_bins = df_bins.drop_duplicates().sort_values(by=[ht.CHR, ht.START])
-
-	# get max and min for each chromosome
-	# df_bins_gr = df_bins.groupby([ht.CHR]).agg({ht.START: [np.min, np.max]}).reset_index()
-	# df_bins_gr.columns = [ht.CHR, "start_amin", "start_amax"]
-	# df_bins_gr["start_amin"] = df_bins_gr.apply(lambda x: max(0, x["start_amin"] - margin_size), axis=1)
-	# df_bins_gr["start_amax"] = df_bins_gr.apply(lambda x: x["start_amax"] + margin_size, axis=1)
-	#
-	# df_bins = pd.concat([df_bins, df_bins_gr[[ht.CHR, "start_amin"]].rename(columns={"start_amin": ht.START})],
-	# 					ignore_index=True)
-	# df_bins = pd.concat([df_bins, df_bins_gr[[ht.CHR, "start_amax"]].rename(columns={"start_amax": ht.START})],
-	# 					ignore_index=True)
-	# df_bins = df_bins.sort_values(by=[ht.CHR, ht.START]).drop_duplicates()
+	df_bins = df_bins.sort_values(by=[ht.CHR, ht.START])
 
 	# rotate with 1 up the start column
 	df_bins_suffix = df_bins.tail(-1)
-	df_bins_suffix = df_bins_suffix.append((df_bins.head(1)), ignore_index=True)
+	df_bins_suffix = pd.concat([df_bins_suffix,df_bins.head(1)], axis=0, ignore_index=True)
 
 	df_bins.reset_index(drop=True, inplace=True)
 	df_bins_suffix.reset_index(drop=True, inplace=True)
 	df_bins = pd.concat([df_bins, df_bins_suffix], axis=1, ignore_index=True)
-	df_bins.columns = [ht.CHR, ht.START, "#chr2", ht.END]
+	df_bins.columns = [ht.CHR, ht.START, ht.CHRH2, ht.END]
 	# keep only rows with same chr and non-negative distance
-	df_bins = df_bins[(df_bins[ht.CHR] == df_bins["#chr2"]) & (abs(df_bins[ht.START] - df_bins[ht.END]) >= 0)]
+	df_bins = df_bins[(df_bins[ht.CHR] == df_bins[ht.CHRH2]) & (abs(df_bins[ht.START] - df_bins[ht.END]) >= 0)]
 
 	# # merge intervals
 	# df_bins[ht.END] = df_bins.apply(lambda x: x[ht.END] if abs(x[ht.START] - x[ht.END]) < 50 else x[ht.END]-1, axis=1)
@@ -190,7 +161,12 @@ def bin_genome(t_collection, r_collection, margin_size=10000):
 
 def read_pyranges(df_t, df_r, bins):
 	"""
-	Load data as pyranges
+	Load data as pyranges.
+
+	Args:
+		df_t (pd.DataFrame): First dataframe containing reconstructed amplicons
+		df_r (pd.DataFrame): Second dataframe containing reconstructed amplicons
+		bins (pd.DataFrame): Genomic bins
 	"""
 	df_t_pr = pr.PyRanges(
 		df_t.rename(columns=rename_columns(df_t.columns.tolist(), p.DICT_COLS), errors="raise", inplace=False))
@@ -200,6 +176,40 @@ def read_pyranges(df_t, df_r, bins):
 		bins.rename(columns=rename_columns(bins.columns.tolist(), p.DICT_COLS), errors="raise", inplace=False))
 
 	return df_t_pr, df_r_pr, df_bins_pr
+
+def overlap_vector(e1, e2, bins):
+	"""
+	Overlap bins and the two reconstructed amplicons files
+
+	Args:
+		e1 (pr.PyRanges):
+		e2 (pr.PyRanges):
+		bins (pr.PyRanges):
+
+	Returns:
+
+		A dataframe containing the overlap between the three intervals.
+		Columns 'bins,e1,e2' show the overlap count between the sample and Chromosome:Start-End
+		Columns 'h1,h2' represent the indicator vectors of 'e1,e2'
+		Column 'bin_enabled' is set to 0 if 'e1' or 'e2' does not overlap with the bin
+
+		header: Chromosome	Start	End	bins	e1	e2	h1	h2	bin_enabled
+
+	"""
+	grs = {n: s for n, s in zip(["bins", "e1", "e2"], [bins, e1, e2])}
+	# Chromosome	Start	End	bins	e1	e2
+	overlaps = pr.count_overlaps(grs)
+	overlaps = overlaps.as_df()
+
+	# everything > 1 set to 1
+	overlaps['h1'] = overlaps['e1'].apply(lambda x: 1 if x >= 1 else 0)
+	overlaps['h2'] = overlaps['e2'].apply(lambda x: 1 if x >= 1 else 0)
+
+	# disable bins which are not covered by amplicons
+	overlaps[ht.BIN_ENABLED] = 1
+	overlaps.loc[(overlaps.h1 == 0) & (overlaps.h2 == 0), ht.BIN_ENABLED] = 0
+
+	return overlaps
 
 def get_bin_len(s_bins):
 	"""
@@ -211,21 +221,27 @@ def get_feature_cn(df_cycles, bins):
 	"""
 	Get copy-number binned using defined intervals
 	"""
+
 	# bins
-	a = BedTool.from_dataframe(bins[[ht.CHR, ht.START, ht.END]])
+	a = BedTool.from_dataframe(bins[[ht.CCHR,
+									 ht.CSTART,
+									 ht.CEND,
+									 ht.BIN_ENABLED]])
 
 	# sum all cn for same bin
-	df_new = df_cycles[[ht.CHR, ht.START, ht.END, ht.CN]].sort_values(by=[ht.CHR, ht.START, ht.END]).groupby(
-		[ht.CHR, ht.START, ht.END]).sum().reset_index()
-	b = BedTool.from_dataframe(df_new[[ht.CHR, ht.START, ht.END, ht.CN]])
+	df_new = df_cycles[[ht.CHR, ht.START, ht.END, ht.CN]].\
+		groupby([ht.CHR, ht.START, ht.END], observed=True, as_index=False).sum()
+	df_new.columns = [ht.CCHR, ht.CSTART, ht.CEND, ht.CN]
+
+	b = BedTool.from_dataframe(df_new[[ht.CCHR, ht.CSTART, ht.CEND, ht.CN]])
 
 	# intersect them
-	o1 = a.intersect(b, wo=True, loj=True).to_dataframe().iloc[:, [0, 1, 2, 6]]
-	o1.columns = [ht.CHR, ht.START, ht.END, ht.CN]
-
+	o1 = a.intersect(b, wo=True, loj=True).to_dataframe().iloc[:, [0, 1, 2, 3, 7]]
+	o1.columns = [ht.CCHR, ht.CSTART, ht.CEND, ht.BIN_ENABLED, ht.CN]
 	o1.loc[o1[ht.CN] == ".", ht.CN] = 0
+
 	o1[ht.CN] = o1.apply(lambda x: round(float(x[ht.CN]),0), axis=1)
-	o1 = o1.groupby([ht.CHR, ht.START, ht.END]).sum().reset_index()
+	o1 = o1.groupby([ht.CCHR, ht.CSTART, ht.CEND, ht.BIN_ENABLED], observed=True, as_index=False).sum()
 
 	return o1
 