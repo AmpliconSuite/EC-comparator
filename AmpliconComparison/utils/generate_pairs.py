fe9a7b3e7596e7cee4a155c12beebc7dc537a146
Madalina Giurgiu <giurgiu.mada@gmail.com>
2025-04-23 10:08:18 +0200

    fix generate pairs

--- AmpliconComparison/utils/generate_pairs.py
+++ AmpliconComparison/utils/generate_pairs.py
@@ -1,6 +1,7 @@
-"""
+'''
 Generate all pairs to compare
-"""
+'''
+
 import ast
 import os
 import argparse
@@ -9,13 +10,26 @@ import subprocess
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
 def get_directory(i,df):
-	return os.path.dirname(df.iloc[i,22])
+	if df.iloc[i,11] != "Not Provided":
+		return os.path.dirname(df.iloc[i,11])
+	else:
+		return os.path.dirname(df.iloc[i,10])
 
 def get_file(i,df):
-	fname = os.path.basename(df.iloc[i,22])
-	sample = fname.split("_AA_results")[0]
-	amplicon = "amplicon" + str(int(df.iloc[i,1]))
-	return f"{sample}_AA_results/{sample}_{amplicon}_cycles.bed", f"{sample}_{amplicon}"
+	if df.iloc[i,11] != "Not Provided" and "_AA_results" in df.iloc[i,11]:
+		#print("11 ", df.iloc[i,11])
+		fname = os.path.basename(df.iloc[i,11])
+		sample = fname.split("_AA_results")[0]
+		amplicon = "amplicon" + str(int(df.iloc[i,1]))
+		return f"{sample}_AA_results/{sample}_{amplicon}_cycles.bed", f"{sample}_{amplicon}"
+	elif df.iloc[i,11] != "Not Provided":
+		#print("11 ", df.iloc[i,11])
+		fname = os.path.basename(df.iloc[i,11]).split(".")[0]
+		return f"{fname}_cycles.bed", fname
+	else:
+		#print("10 ", df.iloc[i,10])
+		fname = os.path.basename(df.iloc[i,10]).split(".")[0]
+		return f"{fname}_cycles.bed", fname
 
 def get_path(i,df, prefix_root):
 	dirname1 = get_directory(i,df)
@@ -26,8 +40,8 @@ def get_path(i,df, prefix_root):
 		return os.path.join(dirname1,filename1), sample1
 
 def overlap(i,j,df,genelist):
-	genes1 = ast.literal_eval(df.iloc[i,5].replace("'","")) + ast.literal_eval(df.iloc[i,6].replace("'",""))
-	genes2 = ast.literal_eval(df.iloc[j,5].replace("'","")) + ast.literal_eval(df.iloc[j,6].replace("'",""))
+	genes1 = list(set(ast.literal_eval(df.iloc[i,4].replace("'","")) + ast.literal_eval(df.iloc[i,5].replace("'","")))
+	genes2 = list(set(ast.literal_eval(df.iloc[j,4].replace("'","")) + ast.literal_eval(df.iloc[j,5].replace("'","")))
 	genes_overlap = set(genes1) & set(genes2)
 	
 	# check if the two sample have at least one gene overlapping
@@ -42,9 +56,9 @@ def overlap(i,j,df,genelist):
 
 def run_subprocess(i,df,project, prefix_root, root, genelist):
 	
-	df_out = pd.DataFrame(columns=["project",
-								   "sample1","sample2",
-								   "amplicon1_name","amplicon2_name",
+	df_out = pd.DataFrame(columns=["project","reference",
+								   "sample1","sample2", "amplicon1_name","amplicon2_name",
+								   "amplicon1_classification","amplicon2_classification",
 								   "amplicon1_complexity","amplicon2_complexity",
 								   "tissueorigin1","tissueorigin2",
 								   "sampletype1","sampletype2",
@@ -56,27 +70,31 @@ def run_subprocess(i,df,project, prefix_root, root, genelist):
 
 		amplicon_path1, amplicon_name1 = get_path(i,df,prefix_root)
 		amplicon_path2, amplicon_name2 = get_path(j,df,prefix_root)
+		sample1=df.iloc[i,0]
+		sample2=df.iloc[j,0]
 
 		if ok:
 
 			new_row = {
 				"project": project,
-				"reference": df.iloc[i,12],
-				"sample1": df.iloc[i,0],
-				"sample2": df.iloc[j,0],
+				"reference": df.iloc[i,7],
+				"sample1": sample1,
+				"sample2": sample2,
+                "amplicon1_name": amplicon_name1,
+                "amplicon2_name": amplicon_name2,
 				"amplicon1_classification": df.iloc[i,3],
 				"amplicon2_classification": df.iloc[j,3],
-				"amplicon1_complexity": df.iloc[j,7],
-				"amplicon2_complexity": df.iloc[j,7],
-				"tissueorigin1": df.iloc[i,13],
-				"tissueorigin2": df.iloc[j,13],
-				"sampletype1": df.iloc[i,14],
-				"sampletype2": df.iloc[j,14],
+				"amplicon1_complexity": df.iloc[i,6],
+				"amplicon2_complexity": df.iloc[j,6],
+				"tissueorigin1": df.iloc[i,8],
+				"tissueorigin2": df.iloc[j,8],
+				"sampletype1": df.iloc[i,9],
+				"sampletype2": df.iloc[j,9],
 				"genes1": genes1,
 				"genes2": genes2,
 				"amplicon1_path": amplicon_path1,
 				"amplicon2_path": amplicon_path2,
-				"outdir": os.path.join(root,f"{amplicon_name1}_{amplicon_name2}") if root else f"{amplicon_name1}_{amplicon_name2}"
+				"outdir": os.path.join(root,f"{sample1}/{amplicon_name1}_vs_{amplicon_name2}") if root else f"{sample1}/{amplicon_name1}_vs_{amplicon_name2}"
 			}
 			df_out.loc[len(df_out)] = new_row
 	return df_out
@@ -87,10 +105,11 @@ def generate_pairs(input,output,project,prefix_root=None,root=None,genelist=None
 	results = []
 	df = pd.read_csv(input, header=0, sep=",",index_col=0)
 	# keep only entries having an amplicon
-	df = df[~df["AA amplicon number"].isna()]
+	columns=["Sample name","AA amplicon number","Feature ID","Classification","Oncogenes","All genes","Complexity score","Reference version","Tissue of origin","Sample type","AA PNG file","AA directory"]
+	df = df.loc[~df["AA amplicon number"].isna(),columns]
  
 	# Use ProcessPoolExecutor to run the subprocesses in parallel
-	with ProcessPoolExecutor(max_workers=10) as executor:
+	with ProcessPoolExecutor(max_workers=30) as executor:
 		# Submit all commands to be run in parallel
 		futures = {
 			executor.submit(run_subprocess, i, df, project, prefix_root, root, genelist): i for i in range(0,df.shape[0])
